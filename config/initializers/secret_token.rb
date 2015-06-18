055d8a3ed3e5566dd0d5b1259dc68ed07d4b15c8
Sanchit Bahal <sanchit.bahal@live.com>
2015-06-18 14:36:48 +0530

    added project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HelloRor::Application.config.secret_token = '295d191cdfd364d538fc6d95251ce846ff8225a38a44fb7f641bc541e864057e8307ffe67f5aeda42ba1b841382dc40eec90cef23455d8440661e830f38f37c1'
